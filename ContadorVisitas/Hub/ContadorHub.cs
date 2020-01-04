1a1a7b32124603887624fa375926c9c8735c6d6e
RaulLH <raullopezhernando@gmail.com>
2020-01-04 19:10:21 +0100

    Agregar archivos de proyecto.

---
+++ ContadorVisitas/Hub/ContadorHub.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ContadorVisitas.Hub
+{
+    public class ContadorHub : Microsoft.AspNet.SignalR.Hub
+    {
+        private static int _visitantes = 0;
+
+        // LADO DEL SERVIDOR
+
+        // Cuando se esta conectado al Servidor (OnConnected())
+        public override async Task OnConnected() 
+        {
+            // Cada vez que un cliente se conecta se incrementa en una unidad nuestra variable
+            Interlocked.Increment(ref _visitantes);
+            await Clients.Others.Message("Nueva Conexion" + Context.ConnectionId + "Total Visitantes" + _visitantes);
+            await Clients.Caller.Message("Hola, Bienvenido....!!!");
+        }
+
+        public override Task OnDisconnected(bool stopCalled) 
+        {
+            // Cada vez que un cliente se desconecta se resta en una unidad nuestra variable
+            Interlocked.Decrement(ref _visitantes);
+            return Clients.All.Messages(Context.ConnectionId + "cerro la conexion" + "Total Visitantes" + _visitantes);
+        }
+
+        public Task Broadcast(string message) 
+        {
+
+            return Clients.All.Message(Context.ConnectionId + "-----" + message);
+        
+        }
+    }
+}
---
+++ ContadorVisitas/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ContadorVisitas.Startup))]
+
+// LADO DEL SERVIDOR - Configuarando mapeo de Proxi con MapSignalR
+namespace ContadorVisitas
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR("/realtime", new Microsoft.AspNet.SignalR.HubConfiguration());
+        }
+    }
+}
